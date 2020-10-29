b2f67d907c78b22f90c616ca388c24181c90826a
xiongaow <627040085@qq.com>
2020-10-29 17:04:20 +0800

    选择照片工具提交

--- app/src/main/java/com/qyai/framapp/MainActivity.java
+++ app/src/main/java/com/qyai/framapp/MainActivity.java
@@ -1,9 +1,9 @@
 package com.qyai.framapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -26,8 +26,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Intent intent=new Intent(MainActivity.this, com.lib.picturecontrol.MainActivity.class);
+                startActivity(intent);
             }
         });
     }
